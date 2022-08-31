9945a3925d49e9db8e3aa182ab432dd4e83625a0
Bruno Farias <brunofelipefariassilva@gmail.com>
2022-08-31 15:25:06 -0300

    criei os menus

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import menu.Menu;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Menu.menu1();
     }
 }
---
+++ src/menu/Menu.java
@@ -0,0 +1,38 @@
+package menu;
+
+public class Menu {
+    public static void menu1() {
+        System.out.println("============================================");
+        System.out.println("Bem-vindo ao sistema de aluguel de veículos");
+        System.out.println("Digite a opção correspondente: ");
+        System.out.println("1 - Cliente");
+        System.out.println("2 - Vendedor");
+        System.out.println("3 - Administrador");
+        System.out.println("0 - Sair do sistema");
+        System.out.println("============================================");
+    }
+    public static void menuCliente1() {
+        System.out.println("VocÊ já tem cadastro? Digite seu email:");
+        System.out.println("Digite 2 caso não tenha cadastro");
+    }
+    public static void menuCliente2() {
+        System.out.println("Digite a opção desejada: ");
+        System.out.println("1 - Alugar um veículo");
+        System.out.println("2 - Devolver um veículo");
+    }
+    
+    public static void menuVendedor1() {
+        System.out.println("Digite a opção desejada: ");
+        System.out.println("1 - Ver os veículos que você alugou");
+        System.out.println("2 - Ver seu salário");
+        System.out.println("3 - Ver seu salário com a comissão atual");
+    }
+
+    public static void menuAdministrador() {
+        System.out.println("Digite a opção desejada: ");
+        System.out.println("1 - Cadastrar um veículo");
+        System.out.println("2 - Remover um veículo");
+        System.out.println("3 - Cadastrar um vendedor");
+        System.out.println("4 - Remover um vendedor");
+    }
+}
