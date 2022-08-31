66ec49183f1c007a34e7365a434aa845b5336fe6
Bruno Farias <brunofelipefariassilva@gmail.com>
2022-08-31 15:43:57 -0300

    adicionei a primeira parte do serviço

--- src/App.java
+++ src/App.java
@@ -1,7 +1,43 @@
+import java.util.Scanner;
+
 import menu.Menu;
+import repository.AdministradorRepository;
+import repository.ClienteRepository;
+import repository.VeiculoRepository;
+import repository.VendedorRepository;
 
 public class App {
     public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    AdministradorRepository admRepository = new AdministradorRepository();
+    ClienteRepository clienteRepository = new ClienteRepository();
+    VendedorRepository vendedorRepository = new VendedorRepository();
+    VeiculoRepository veiculoRepository = new VeiculoRepository();
+    boolean continua = true;
+    do{
         Menu.menu1();
+        int opcao1 = sc.nextInt();
+        switch(opcao1) {
+            case 1:
+                Menu.menuCliente1();
+                int opcao2 = sc.nextInt();
+                break;
+            case 2:
+                Menu.menuVendedor1();
+                opcao2 = sc.nextInt();
+                break;
+            case 3:
+                Menu.menuAdministrador();
+                opcao2 = sc.nextInt();
+            break;
+            case 0:
+                continua = false;
+                break;
+            default:
+                System.out.println("Opção inválida! Tente novamente");
+                break;
+        }
+    }while(continua);
+
     }
 }
--- src/repository/VeiculoRepository.java
+++ src/repository/VeiculoRepository.java
@@ -3,7 +3,6 @@ package repository;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import model.Veiculo;
